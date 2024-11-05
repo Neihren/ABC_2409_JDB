0e2748abec44407e0c9f3502066d8a389e3011ba
Neihren <185206470+Neihren@users.noreply.github.com>
2024-11-05 16:50:13 +0100

    45

--- crmExos/src/tpYaourts.java
+++ crmExos/src/tpYaourts.java
@@ -1,47 +1,51 @@
+import java.security.KeyStore.Entry;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class tpYaourts {
     public static void main(String[] args) {
-        String[] json = { "rouge", "rouge", "jaune", "bleu", "jaune", "rouge", "jaune" };
-        HashMap<String, Integer> colors = new HashMap<>();
+        String[] json = { "rouge", "rouge", "jaune", "bleu", "jaune", "rouge", "jaune", "bleu", "arc" };
+        LinkedHashMap<String, Integer> colors = new LinkedHashMap<>();
         int count = 0;
-        int currentMaxValue = Integer.MIN_VALUE;
-        final List<String> resultList = new ArrayList<String>();
-        
-        
+        String result = "";
         for (int i = 0; i < json.length; i++) {
             if (colors.containsKey(json[i])) {
                 count = colors.get(json[i]);
-                colors.put(json[i],count + 1);
-            }
-            else {
+                colors.put(json[i], count + 1);
+            } else {
                 colors.put(json[i], 1);
             }
         }
-        HashMap<String, Integer> reverseColors = new HashMap<>();
-        for (Map.Entry<String, Integer> entry : colors.entrySet()) {
-            reverseColors.put(entry.getKey(), entry.getValue());
-        }
-        
-        for (Map.Entry<String, Integer> entry : reverseColors.entrySet()) {
-            if (entry.getValue() > currentMaxValue) {
-                resultList.clear();
-                resultList.add(entry.getKey());
-                currentMaxValue = entry.getValue();
-            }
-            else if (entry.getValue() == currentMaxValue) {
-                resultList.add(entry.getKey());
-            }
-        }
-        System.out.println(resultList);
+
+        Map.Entry<String, Integer> m = colors.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .findFirst()
+                .get();
+System.out.println(m);
+        Map.Entry<String, Integer> n = colors.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .skip(1)
+                .findFirst()
+                .get();
+System.out.println(n);
+
+
+        // for (Map.Entry<String, Integer> entry : colors.entrySet()) {
+        // if (entry.getValue() > currentMaxValue) {
+        // resultList.clear();
+        // resultList.add(entry.getKey());
+        // currentMaxValue = entry.getValue();
+        // } else if (entry.getValue() == currentMaxValue) {
+        // resultList.add(entry.getKey());
+        // }
+        // }
+
         for (String i : colors.keySet()) {
             System.out.println("key: " + i + " value: " + colors.get(i));
         }
-        System.out.println(colors.size());
-        System.out.println(colors.keySet());
-        System.out.println(reverseColors.keySet());
     }
 }
