7f0dab0a336b650f41d7d7fc14cb6c3ace97187c
Neihren <185206470+Neihren@users.noreply.github.com>
2024-11-12 08:21:34 +0100

    Correctif pour somme ne nécéssitant pas un rendu

--- crmExos/src/exo03_08.java
+++ crmExos/src/exo03_08.java
@@ -19,39 +19,44 @@ public class exo03_08 {
             sum = sum.add(price);
         }
         System.out.println("\nLe client doit payer : " + String.format("%.2f", sum) + " Euros.");
-        nb = (int) sum.doubleValue() / 5 + 1;
+        if (sum.doubleValue() % 5 == 0)
+            nb = (int) sum.doubleValue() / 5;
+        else
+            nb = (int) sum.doubleValue() / 5 + 1;
         change = (BigDecimal.valueOf(nb * 5)).subtract(sum);
         System.out.println("Le client doit donner " + nb + " billets de 5 Euros soit " + (nb * 5) + " Euros.");
         System.out.println("Rendu monnaie : " + String.format("%.2f", change) + " Euros.");
-        while (change.doubleValue() != 0) {
-            if (change.doubleValue() >= 2) {
-                change = change.subtract(BigDecimal.valueOf(2));
-                nbTwo++;
-            } else if (change.doubleValue() >= 1) {
-                change = change.subtract(BigDecimal.valueOf(1));
-                nbOne++;
-            } else if (change.doubleValue() >= 0.5) {
-                change = change.subtract(BigDecimal.valueOf(0.5));
-                nbFifty++;
-            } else if (change.doubleValue() >= 0.2) {
-                change = change.subtract(BigDecimal.valueOf(0.2));
-                nbTwenty++;
-            } else if (change.doubleValue() >= 0.1) {
-                change = change.subtract(BigDecimal.valueOf(0.1));
-                nbTen++;
+        if (change.doubleValue() > 0) {
+            while (change.doubleValue() != 0) {
+                if (change.doubleValue() >= 2) {
+                    change = change.subtract(BigDecimal.valueOf(2));
+                    nbTwo++;
+                } else if (change.doubleValue() >= 1) {
+                    change = change.subtract(BigDecimal.valueOf(1));
+                    nbOne++;
+                } else if (change.doubleValue() >= 0.5) {
+                    change = change.subtract(BigDecimal.valueOf(0.5));
+                    nbFifty++;
+                } else if (change.doubleValue() >= 0.2) {
+                    change = change.subtract(BigDecimal.valueOf(0.2));
+                    nbTwenty++;
+                } else if (change.doubleValue() >= 0.1) {
+                    change = change.subtract(BigDecimal.valueOf(0.1));
+                    nbTen++;
+                }
             }
+            System.out.println("Répartition de la monnaie à restituer au client :");
+            if (nbTwo > 0)
+                System.out.println("- " + nbTwo + " pièce(s) de 2 Euros");
+            if (nbOne > 0)
+                System.out.println("- " + nbOne + " pièce(s) de 1 Euros");
+            if (nbFifty > 0)
+                System.out.println("- " + nbFifty + " pièce(s) de 0.50 Euros");
+            if (nbTwenty > 0)
+                System.out.println("- " + nbTwenty + " pièce(s) de 0.20 Euros");
+            if (nbTen > 0)
+                System.out.println("- " + nbTen + " pièce(s) de 0.10 Euros");
         }
-        System.out.println("Répartition de la monnaie à restituer au client :");
-        if (nbTwo > 0)
-            System.out.println("- " + nbTwo + " pièce(s) de 2 Euros");
-        if (nbOne > 0)
-            System.out.println("- " + nbOne + " pièce(s) de 1 Euros");
-        if (nbFifty > 0)
-            System.out.println("- " + nbFifty + " pièce(s) de 0.50 Euros");
-        if (nbTwenty > 0)
-            System.out.println("- " + nbTwenty + " pièce(s) de 0.20 Euros");
-        if (nbTen > 0)
-            System.out.println("- " + nbTen + " pièce(s) de 0.10 Euros");
         scan.close();
     }
 }
