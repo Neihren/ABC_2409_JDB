de1a223ed01e349030408421b7f5eb9b68d1ca59
Neihren <185206470+Neihren@users.noreply.github.com>
2024-11-15 09:58:35 +0100

    Ajout de l'affichage

--- crmExos/src/exo04_06.java
+++ crmExos/src/exo04_06.java
@@ -6,10 +6,17 @@ public class exo04_06 {
         int[] tab = { 4, 8, 7, 12 };
         int[] tab2 = { 3, 6 };
         int result = 0;
-        for (int i = 0; i < tab2.length; i++)
-            for (int j = 0; j < tab.length; j++)
+        String display = "";
+        for (int i = 0; i < tab2.length; i++) {
+            for (int j = 0; j < tab.length; j++) {
+                display += tab2[i] + " * " + tab[j];
                 result = result + tab[j] * tab2[i];
-        System.out.println("Le résultat est : " + result);
+                if (j != tab.length-1 && i != tab2.length) {
+                    display += " + ";
+                }
+            }
+        }
+        System.out.println("Le résultat est " + display + " = " + result);
         // System.out.println(Arrays.toString(tab));
     }
 }
