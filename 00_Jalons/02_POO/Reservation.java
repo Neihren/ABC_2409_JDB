df4f75e07b156eed356cf9ce43f2f95806b57847
Neihren <185206470+Neihren@users.noreply.github.com>
2025-02-05 14:04:45 +0100

    Commit non final

---
+++ 00_Jalons/02_POO/Reservation.java
@@ -0,0 +1,115 @@
+import java.util.List;
+
+public class Reservation {
+    private String nom;
+    private String prenom;
+    private int age;
+    private double prixFinale;
+    private boolean business;
+    private int numeroVol;
+    private String destination;
+
+    public Reservation() {
+        this.nom = "";
+        this.prenom = "";
+        this.age = 0;
+        this.prixFinale = 0;
+        this.business = false;
+        this.numeroVol = 0;
+        this.destination = "";
+    }
+
+    public Reservation(String nom, String prenom, int age, double prixFinale, boolean business, int numeroVol, String destination) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+        this.prixFinale = prixFinale;
+        this.business = business;
+        this.numeroVol = numeroVol;
+        this.destination = destination;
+    }
+
+    public Reservation(Reservation reservationACloner) {
+        this.nom = reservationACloner.nom;
+        this.prenom = reservationACloner.prenom;
+        this.age = reservationACloner.age;
+        this.prixFinale = reservationACloner.prixFinale;
+        this.business = reservationACloner.business;
+        this.numeroVol = reservationACloner.numeroVol;
+        this.destination = reservationACloner.destination;
+    }
+
+    public String toString() {
+        return "Reservation [nom=" + nom + ", prenom=" + prenom + ", age=" + age + ", prixFinale=" + prixFinale + ", business=" + business + ", numeroVol=" + numeroVol + ", destination=" + destination + "]";
+    }
+
+    public boolean estCeComplet(Vol vol) {
+        if (vol.getNbPlacesOccupee() == vol.getNbPlacesTotale()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double reductionAge(Vol vol) {
+        double prixReduit = vol.getTarif();
+        if (this.age < 66) {
+            prixReduit -= vol.getTarif() / (this.age * 0.1);
+        } else {
+            prixReduit -= vol.getTarif() / (this.age * 0.05)    ;
+        }
+        if (prixReduit <= 400) {
+            return 400;
+        } else {
+            return prixReduit;
+        }
+    }
+
+    public double changePrixPlace(Vol vol) {
+        if (vol.getNbPlacesOccupee() * 100 / vol.getNbPlacesTotale() >= 80) {
+            return vol.getTarif() * 1.2;
+        } else if (vol.getNbPlacesOccupee() * 100 / vol.getNbPlacesTotale() <= 20) {
+            return vol.getTarif() * 0.8;
+        } else {
+            return vol.getTarif();
+        }
+    }
+
+    public double augmentationBusiness(Vol vol) {
+        if (this.business) {
+            return vol.getTarif() * 1.7;
+        } else {
+            return vol.getTarif();
+        }
+    }
+
+    public double minimumPrixFinale(Vol vol) {
+        if (this.prixFinale <= 500) {
+            return 500;
+        } else {
+            return this.prixFinale;
+        }
+    }
+
+    public double prixFinale(Vol vol) {
+        return vol.getTarif() - this.reductionAge(vol) - this.augmentationBusiness(vol) - this.changePrixPlace(vol);
+    }
+
+    public Vol ChoisirUnVolParId(List<Vol> vols) {
+        for (Vol vol : vols) {
+            if (vol.getId() == this.numeroVol) {
+                return vol;
+            }
+        }
+        return null;
+    }
+
+    public Vol ChoisirUnVolParDestination(List<Vol> vols) {
+        for (Vol vol : vols) {
+            if (vol.getDestination().equals(this.destination)) {
+                return vol;
+            }
+        }
+        return null;
+    }
+}
---
+++ 00_Jalons/02_POObis/Main.java
@@ -0,0 +1,40 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Vol> vols = new ArrayList<>();
+        Vol vol1 = new Vol(244, "New York", 1000.0, 210, 89);
+        Vol vol2 = new Vol(365, "Johannesburg", 1200.25, 180, 50);
+        Vol vol3 = new Vol(777, "Tokyo", 1500.50, 200, 190);
+        Vol vol4 = new Vol(123, "Rio de Janeiro", 1300.75, 210, 210);
+        Vol vol5 = new Vol(457, "Istanbul", 900, 210, 3);
+        Vol vol6 = new Vol(666, "Enfer", 1000, 210, 10);
+        vols.add(vol1);
+        vols.add(vol2);
+        vols.add(vol3);
+        vols.add(vol4);
+        vols.add(vol5);
+        vols.add(vol6);
+        for (Vol vol : vols) {
+            System.out.println(vol.toString());
+        }
+        Reservation reservation1 = new Reservation("Wick", "John", 45, 0, false, 666, "Enfer");
+        System.out.println(reservation1.ChoisirUnVolParDestination(vols).toString());
+        System.out.println(reservation1.ChoisirUnVolParId(vols).toString());
+        System.out.println(reservation1.toString());
+
+        //System.out.println(reservation1.reductionAge(reservation1.ChoisirUnVolParId(vols)));
+        System.out.println(reservation1.changePrixPlace(reservation1.ChoisirUnVolParId(vols)));
+        System.out.println(reservation1.augmentationBusiness(reservation1.ChoisirUnVolParId(vols)));
+
+        if (!reservation1.estCeComplet(reservation1.ChoisirUnVolParDestination(vols))) {
+            System.out.println(reservation1.prixFinale(reservation1.ChoisirUnVolParId(vols)));
+        } else {
+            System.out.println("Le vol est complet");
+        }
+
+    }
+}
